228962ceea610981dd95c6f39302244222901413
manhducit05 <manhducit05@gmail.com>
2024-10-20 19:59:01 +0700

    complete features

--- middleware/admin/auth.middleware.js
+++ middleware/admin/auth.middleware.js
@@ -1,13 +1,26 @@
-const requireAuth = (req, res, next) => {
+const Account = require("../../models/account.model")
+const Role = require("../../models/role.model")
+
+const requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
   console.log(token);
   if (!token) {
-      res.redirect('/admin/auth/login');
-  } else {
-      next();
+    res.redirect('/admin/auth/login');
   }
-};
+  else {
+    const user = await Account.findOne({
+      token: token
+    })
+    user.password = ""
+    const role = await Role.findOne({
+      _id: user.roleID
+    })
+    res.locals.user = user
+    res.locals.permission = role.permission
+    next()
+  }
+}
 
-  module.exports = {
-    requireAuth
-  }   
+module.exports = {
+  requireAuth
+}   
